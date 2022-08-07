9b2f23b94bb76ff9cdad0300a6ccc2486d2dfa12
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-08-07 18:50:03 +1200

    axios to fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios'
 
 function App() {
+
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState("")
+  const [previousPageUrl, setPreviousPageUrl] = useState("")
+
+
+
+  useEffect(() => {
+    axios.get(currentPageUrl).then(res => {
+      setNextPageUrl(res.data.next)
+      setPreviousPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
+
+
+
   return (
     <div className="App">
-
+      <PokemonList pokemon={pokemon} />
     </div>
   );
 }
