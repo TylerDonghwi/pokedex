d4554eeaf2f72c68fa39f84f7977594155767cbb
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-08-08 13:13:43 +1200

    .

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './pokemon.css'
 
 export default function pokemonList({ pokemon }) {
+    console.log(pokemon.map(p => (
+        p
+    )))
+
     return (
         <div>
             {pokemon.map(p => (
-                <div key={p}>{p}</div>
+                <div className='pokemon' key={p}>{p}</div>
             ))}
         </div>
     )
